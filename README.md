da88c769ac67eef63d77f7872b94b3b6f4a7dc45
ElChicoEevee <kirbyyeray@hotmail.com>
2024-12-22 03:43:00 +0100

    backup + adding more features

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Pokemon-Switch-V2-Model-Importer-Blender
-A Python script for Blender to import/export models from Pokémon Switch Games
+# Pokémon Switch V2 Blender Addon 3.3.x and WIP 4.x.x
+A Python script for Blender to import models from Pokémon Games in Nintendo Switch (VERY WIP)
+
+HUGE thanks to Random Talking Bush for making the original maxscript for this
+https://www.vg-resource.com/thread-29836.html
+
+The formatting of this script is a bit wonky, but that's because I wanted to make it
+look as close to the original maxscript as possible.
+That way, it's easier to compare the two and see what's different.
+
+Pokémon Scarlet Violet Raw Files with PNGs
+
+Unique Trainers (model_uq) https://drive.google.com/file/d/10BeIPdE5Yw_lJPHMLd00l6UR8Ujn3Hpf/view?usp=sharing
+
+All Pokémon from SCVI https://drive.google.com/file/d/1N8NbyD1kh1LI6rSTbtG2wVdjQ47oJIAM/view?usp=sharing
+
+Blender script originally made/ported by @SomeKitten 
---
+++ __init__.py
@@ -0,0 +1,173 @@
+"""
+    Init for GFBANM Importer addon.
+"""
+import os
+import sys
+import subprocess
+import bpy
+from bpy.props import *
+from bpy.utils import register_class, unregister_class
+from bpy_extras.io_utils import ImportHelper
+
+bl_info = {
+    "name": "Pokémon Switch V2 (.TRMDL)",
+    "author": "Scarlett/SomeKitten & ElChicoEevee",
+    "version": (2, 0, 0),
+    "blender": (3, 3, 0),
+    "location": "File > Import",
+    "description": "Blender addon for import Pokémon Switch TRMDL",
+    "warning": "",
+    "category": "Import",
+}
+
+
+class PokeSVImport(bpy.types.Operator, ImportHelper):
+    bl_idname = "custom_import_scene.pokemonscarletviolet"
+    bl_label = "Import"
+    bl_options = {'PRESET', 'UNDO'}
+    filename_ext = ".trmdl"
+    filter_glob: StringProperty(
+            default="*.trmdl",
+            options={'HIDDEN'},
+            maxlen=255,
+    )
+    filepath = bpy.props.StringProperty(subtype='FILE_PATH',)
+    files = CollectionProperty(type=bpy.types.PropertyGroup)
+    rare: BoolProperty(
+            name="Load Shiny",
+            description="Uses rare material instead of normal one",
+            default=False,
+            )
+    multiple: BoolProperty(
+            name="Load All Folder",
+            description="Uses rare material instead of normal one",
+            default=False,
+            )
+    loadlods: BoolProperty(
+            name="Load LODS",
+            description="Uses rare material instead of normal one",
+            default=False,
+            )
+    bonestructh: BoolProperty(
+            name="Bone Extras (WIP)",
+            description="Bone Extras (WIP)",
+            default=False,
+            )
+    def draw(self, context):
+        layout = self.layout
+
+        box = layout.box()
+        box.prop(self, 'rare')
+        
+        box = layout.box()
+        box.prop(self, 'multiple')
+        
+        box = layout.box()
+        box.prop(self, 'loadlods')
+
+        box = layout.box()
+        box.prop(self, 'bonestructh')
+        
+
+    def execute(self, context: bpy.types.Context):
+        if not attempt_install_flatbuffers(self):
+            self.report({"ERROR"}, "Failed to install flatbuffers library using pip. "
+                                   "To use this addon, put Python flatbuffers library folder "
+                                   "to this path: " + get_site_packages_path() + ".")
+            return {"CANCELLED"}
+        from .PokemonSwitch import from_trmdlsv
+        directory = os.path.dirname(self.filepath)
+        if self.multiple == False:
+            filename = os.path.basename(self.filepath)        
+            f = open(os.path.join(directory, filename), "rb")
+            from_trmdlsv(directory, filename, self.rare, self.loadlods, self.bonestructh)
+            f.close()
+            return {'FINISHED'}  
+        else:
+            file_list = sorted(os.listdir(directory))
+            obj_list = [item for item in file_list if item.endswith('.trmdl')]
+            for item in obj_list:
+                from_trmdlsv(directory, item, self.rare, self.loadlods, self.bonestructh)
+                f.close()
+            return {'FINISHED'}
+
+    @classmethod
+    def poll(cls, context: bpy.types.Context):
+        """
+        Checking if operator can be active.
+        :param context: Blender's Context.
+        :return: True if active, False otherwise.
+        """
+        return True
+
+def menu_func_import(operator: bpy.types.Operator, context: bpy.types.Context):
+    """
+    Function that adds GFBANM import operator.
+    :param operator: Blender's operator.
+    :param context: Blender's Context.
+    :return:
+    """
+    operator.layout.operator(PokeSVImport.bl_idname, text="Pokémon Trinity Model (.trmdl)")
+
+
+def register():
+    """
+    Registering addon.
+    """
+    register_class(PokeSVImport)
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+
+
+def unregister():
+    """
+    Unregistering addon.
+    :return:
+    """
+    unregister_class(PokeSVImport)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+
+
+def attempt_install_flatbuffers(operator: bpy.types.Operator = None) -> bool:
+    """
+    Attempts installing flatbuffers library if it's not installed using pip.
+    :return: True if flatbuffers was found or successfully installed, False otherwise.
+    """
+    if are_flatbuffers_installed():
+        return True
+    target = get_site_packages_path()
+    subprocess.call([sys.executable, "-m", 'ensurepip'])
+    subprocess.call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+    subprocess.call(
+        [sys.executable, "-m", "pip", "install", "--upgrade", "flatbuffers", "-t", target])
+    if are_flatbuffers_installed():
+        if operator is not None:
+            operator.report({"INFO"},
+                            "Successfully installed flatbuffers library to " + target + ".")
+        else:
+            print("Successfully installed flatbuffers library to " + target + ".")
+        return True
+    return False
+
+
+def are_flatbuffers_installed() -> bool:
+    """
+    Checks if flatbuffers library is installed.
+    :return: True or False.
+    """
+    try:
+        import flatbuffers
+    except ModuleNotFoundError:
+        return False
+    return True
+
+
+def get_site_packages_path():
+    """
+    Returns file path to lib/site-packages folder.
+    :return: File path to lib/site-packages folder.
+    """
+    return os.path.join(sys.prefix, "lib", "site-packages")
+
+
+if __name__ == "__main__":
+    register()
